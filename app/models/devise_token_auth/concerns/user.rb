75a3c60406ae0f6c3d0deb3178877ba5b8fc178a
poctek <poctek5@gmail.com>
2016-12-22 17:18:12 +0300

    Add if instead of unless -_-

--- app/models/devise_token_auth/concerns/user.rb
+++ app/models/devise_token_auth/concerns/user.rb
@@ -257,7 +257,7 @@ module DeviseTokenAuth::Concerns::User
   def destroy_expired_tokens
     if self.tokens
       self.tokens.delete_if do |cid, v|
-        unless v
+        if v
           expiry = v[:expiry] || v["expiry"]
           DateTime.strptime(expiry.to_s, '%s') < Time.now
         end
